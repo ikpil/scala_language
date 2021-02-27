c2290d12d5a4fba3f3c9cf3b171cc51fd8a0c030
ikpil <ikpil0@gmail.com>
2021-02-28 00:09:24 +0900

    1. 암시적 타입 변환 방법 숙지

--- src/main/scala/com/ikpil/scala/Tutorial006.scala
+++ src/main/scala/com/ikpil/scala/Tutorial006.scala
@@ -3,11 +3,15 @@ package com.ikpil.scala
 import com.ikpil.scala.sample.Rational
 
 object Tutorial006 extends App {
+  // 암시적 타입 변환
+  implicit def intToRational(x: Int): Rational = new Rational(x)
+
   val x = new Rational(1, 2)
   println(x)
 
   val y = new Rational(2, 3)
   println(y)
+
 
   println("연산자 정의가 작동 되는지 확인")
   val z1 = x + y
@@ -19,7 +23,11 @@ object Tutorial006 extends App {
   val z3 = (z1 + z2) * x
   val z4 = z1 + z2 * x
   println(z3)
-  println(z3)
+  println(z4)
+
+  println("암시적 타입 변환 테스트")
+  val z5 = 2 + y
+  println(z5)
 
   println("클래스 메소드 호출 확인")
   val c = x lessThan y
