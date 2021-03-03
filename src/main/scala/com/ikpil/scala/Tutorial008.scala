7384e93c2c6a444743f46838a8182e0523cbfdc4
ikpil <ikpil0@gmail.com>
2021-03-04 08:40:19 +0900

    일단 함수화 시킴

--- src/main/scala/com/ikpil/scala/Tutorial008.scala
+++ src/main/scala/com/ikpil/scala/Tutorial008.scala
@@ -4,8 +4,18 @@ import com.ikpil.scala.sample.LongLines
 
 // 함수와 클로저에 대해서 알아 본다
 object Tutorial008 extends App {
-  val argsV2 = Array("50", "src/main/scala/com/ikpil/scala/Tutorial007.scala", "src/main/scala/com/ikpil/scala/Tutorial008.scala");
-  val width = argsV2(0).toInt
-  for (arg <- argsV2.drop(1))
-    LongLines.processFile(arg, width)
+
+  checkMethod()
+
+  def checkMethod() = {
+    val argsV2 = Array(
+      "50",
+      "src/main/scala/com/ikpil/scala/Tutorial007.scala",
+      "src/main/scala/com/ikpil/scala/Tutorial008.scala"
+    );
+
+    val width = argsV2(0).toInt
+    for (arg <- argsV2.drop(1))
+      LongLines.processFile(arg, width)
+  }
 }
