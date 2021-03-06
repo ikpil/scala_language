333eca967e208ef1ab4d4ad36d1b420b107c3a20
ikpil <ikpil0@gmail.com>
2021-03-07 02:30:55 +0900

    지역 함수를 사용해 보았다.

--- src/main/scala/com/ikpil/scala/sample/LongLines.scala
+++ src/main/scala/com/ikpil/scala/sample/LongLines.scala
@@ -6,11 +6,13 @@ object LongLines {
   def processFile(filename: String, width: Int) = {
     val source = Source.fromFile(filename)
     for (line <- source.getLines())
-      processLine(filename, width, line)
-  }
+      processLine(line)
 
-  private def processLine(filename: String, width: Int, line: String) = {
-    if (line.length > width)
-      println(filename + ": " + line.trim)
+    // 스칼라에서는 지역함수라고 하여, 함수 안에 함수를 만들수 있다.
+    def processLine(line: String) = {
+      // 지역함수를 감싸는 블럭의 변수, 파라미터등에 접근 할 수 있다
+      if (line.length > width)
+        println(filename + ": " + line.trim)
+    }
   }
 }
