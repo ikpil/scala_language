260c115f7e348ecb580dfb371d794cbe89623444
ikpil <ikpil0@gmail.com>
2021-03-08 23:35:50 +0900

    스칼라에서 함수가 1급 계층 함수라는 것을 알았다

--- src/main/scala/com/ikpil/scala/Tutorial008.scala
+++ src/main/scala/com/ikpil/scala/Tutorial008.scala
@@ -5,7 +5,8 @@ import com.ikpil.scala.sample.LongLines
 // 함수와 클로저에 대해서 알아 본다
 object Tutorial008 extends App {
 
-  checkMethod()
+  // checkMethod()
+  firstClassFunction()
 
   def checkMethod() = {
     val argsV2 = Array(
@@ -17,5 +18,31 @@ object Tutorial008 extends App {
     val width = argsV2(0).toInt
     for (arg <- argsV2.drop(1))
       LongLines.processFile(arg, width)
+  }
+
+  def firstClassFunction() = {
+    // 함수 리터럴, 함수 값은 변수에 저장할 수 있음을 보여 준다.
+    var fn = (x: Int) => x + 1
+    println(fn(10))
+
+    fn = (x: Int) => x + 9999
+    println(fn(1))
+
+    fn = (x: Int) => {
+      println("We")
+      println("are")
+      println("here!")
+      x + 1
+    }
+
+    println(fn(1))
+
+    // 모든 컬렉션에는 foreach 가 있음
+    val someNumbers = List(-11, -10, -5, 0, 5, 10)
+    someNumbers.foreach((x: Int) => println(x))
+
+    // 모든 컬렉션에는 filter 가 있음
+    val filtered = someNumbers.filter((x: Int) => x > 0)
+    println(filtered)
   }
 }
