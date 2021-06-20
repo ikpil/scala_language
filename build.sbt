1b38d3179ad1eea6914181e9182acb926a246b20
ikpil <ikpil0@gmail.com>
2021-06-20 21:03:21 +0900

    튜토리얼 9 - 스칼라를 gradle 에서 빌드/실행 할 수 있게 수정

--- build.sbt
+++
@@ -1,5 +0,0 @@
-name := "scala_language"
-
-version := "0.1"
-
-scalaVersion := "2.13.4"
---
+++ src/main/scala/com/ikpil/scala/App.scala
@@ -0,0 +1,7 @@
+package com.ikpil.scala
+
+object App {
+  def main(args: Array[String]): Unit = {
+    Tutorial009.main(args)
+  }
+}
