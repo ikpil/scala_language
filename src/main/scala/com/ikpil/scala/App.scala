777be5e0b4812ee5d00738c5f28c4e73c3c814bf
ikpil <ikpil0@gmail.com>
2021-06-20 21:05:31 +0900

    튜토리얼 9 - 스칼라를 gradle 에서 빌드/실행 할 수 있게 수정

--- src/main/scala/com/ikpil/scala/App.scala
+++
@@ -1,7 +0,0 @@
-package com.ikpil.scala
-
-object App {
-  def main(args: Array[String]): Unit = {
-    Tutorial009.main(args)
-  }
-}
---
+++ src/main/scala/com/ikpil/scala/TutorialApp.scala
@@ -0,0 +1,5 @@
+package com.ikpil.scala
+
+object TutorialApp extends App {
+  Tutorial009.main(args)
+}
