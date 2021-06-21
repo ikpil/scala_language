2576c36a612f0ec7acc6de55ded69ab47d53f567
ikpil <ikpil0@gmail.com>
2021-06-22 06:47:14 +0900

    챕터10 - 클래스 만들기

---
+++ src/main/scala/com/ikpil/scala/sample/ArrayElement.scala
@@ -0,0 +1,5 @@
+package com.ikpil.scala.sample
+
+class ArrayElement(conts: Array[String]) extends Element {
+  def contents: Array[String] = conts
+}
---
+++ src/main/scala/com/ikpil/scala/sample/Element.scala
@@ -0,0 +1,10 @@
+package com.ikpil.scala.sample
+
+// 추상 클래스
+abstract class Element {
+  def contents: Array[String]
+
+  def height: Int = contents.length // 파라미터 없는 메소드
+
+  def width: Int = if (height == 0) 0 else contents(0).length
+}
