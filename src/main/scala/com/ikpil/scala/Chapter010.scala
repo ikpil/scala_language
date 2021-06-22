3e21c99f501fd12a14e6684b235af87275ca9c92
ikpil <ikpil0@gmail.com>
2021-06-23 08:39:23 +0900

    챕터10 - 클래스 테스트

--- src/main/scala/com/ikpil/scala/Chapter010.scala
+++ src/main/scala/com/ikpil/scala/Chapter010.scala
@@ -1,8 +1,20 @@
 package com.ikpil.scala
 
+import com.ikpil.scala.sample.{ArrayElement, Element}
+
 /** *
  * 챕터 10에서는 상속과 구성에 대해서 다룬다
  */
 object Chapter010 extends App {
-  println("hello tutorial 10")
+  classExtendTest()
+
+  def classExtendTest(): Unit = {
+    // 생성
+    val ae = new ArrayElement(Array("hello", "world"))
+    println("ArrayElement width:" + ae.width);
+
+    // 서브클래스로 받는다
+    val e: Element = ae
+    println("Element width:" + e.width);
+  }
 }
