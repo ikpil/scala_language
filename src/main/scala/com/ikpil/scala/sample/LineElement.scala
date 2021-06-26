1e854eb6942250b3e5524507511ab4048d763bf2
ikpil <ikpil0@gmail.com>
2021-06-26 11:08:35 +0900

    챕터10 - override 가 유무가 의무이므로 깨지기 쉬운 기반 클래스(fragile base class) 를 그나마 해소시켜준다

--- src/main/scala/com/ikpil/scala/sample/LineElement.scala
+++ src/main/scala/com/ikpil/scala/sample/LineElement.scala
@@ -2,7 +2,11 @@ package com.ikpil.scala.sample
 
 // 슈퍼클래스의 생성자 호출을 보여준다.
 class LineElement(s: String) extends ArrayElement(Array(s)) {
+  // width, height 에 override
   override def width: Int = s.length
 
   override def height: Int = 1
+
+  // 스칼라에서는 override 유무가 필수이므로, 깨지기 쉬운 기반 클래스(fragile base class) 문제를 조금이나마 해소시켜준다
+  //override def abcde: Int = 2
 }
